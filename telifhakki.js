573d28ff18c9e4d244129fbaf192177f71b008f2
serkancelik1994 <serkancelik1994@gmail.com>
2019-06-30 03:18:17 +0300

    Create telifhakki.js

---
+++ telifhakki.js
@@ -0,0 +1,9 @@
+window['onload'] = function() {
+    var _0xc4edx3 = document['getElementById']('gdcontent');
+    if (_0xc4edx3 == null) {
+        window['location']['href'] = 'https://www.katreimatem.com'
+    };
+    _0xc4edx3['setAttribute']('href', 'https://www.katreimatem.com/');
+    _0xc4edx3['setAttribute']('rel', 'nofollow');
+    _0xc4edx3['innerHTML'] = 'Bir Serkan Çelik <a href=\'https://www.katreimatem.com\' rel=\'nofollow\' target=\'_blank\'>sitesidir.</a>';
+};
